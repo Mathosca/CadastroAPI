2315d687f1c1a2fd957c9102ddd4b3776b1ac7aa
Matheus Gomes <you@example.com>
2025-03-19 14:32:03 -0300

    first commit

---
+++ src/main/java/dev/matheus/CadastroAPIJava10x/CadastroApiJava10xApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus.CadastroAPIJava10x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApiJava10xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApiJava10xApplication.class, args);
+	}
+
+}
