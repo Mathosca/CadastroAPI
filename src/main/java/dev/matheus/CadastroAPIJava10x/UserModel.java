0ef4558b9234ae8a2ab7d614da9159b4252afac7
Matheus Gomes <you@example.com>
2025-03-19 19:28:41 -0300

    Criando a tabela de cadastro

---
+++ src/main/java/dev/matheus/CadastroAPIJava10x/UserModel.java
@@ -0,0 +1,49 @@
+package dev.matheus.CadastroAPIJava10x;
+
+import jakarta.persistence.*;
+
+@Entity  //@Entity transforma a classe em uma entidade do DB
+@Table(name = "tb_cadastro")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public UserModel() {
+    }
+
+    public UserModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
