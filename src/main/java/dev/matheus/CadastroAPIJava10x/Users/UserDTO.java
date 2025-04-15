6c41c2f6d72f17b92e31d9227ea71df8dff537a1
Matheus Gomes <you@example.com>
2025-04-15 13:31:48 -0300

    Chore: Includes the version of the DTO by entity and manually maps in NinjaMapper the entity x dto paths

--- src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserDTO.java
+++ src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserDTO.java
@@ -1,4 +1,21 @@
 package dev.matheus.CadastroAPIJava10x.Users;
 
+import dev.matheus.CadastroAPIJava10x.Tasks.TasksModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String mail;
+    private int age;
+    private String function;
+    private String role;
+    private TasksModel tasks;
+
 }
--- src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserMapper.java
+++ src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserMapper.java
@@ -1,4 +1,34 @@
 package dev.matheus.CadastroAPIJava10x.Users;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public UserModel map(UserDTO userDTO) {
+        UserModel userModel = new UserModel();
+        userModel.setId(userDTO.getId());
+        userModel.setName(userDTO.getName());
+        userModel.setMail(userDTO.getMail());
+        userModel.setAge(userDTO.getAge());
+        userModel.setRole(userDTO.getRole());
+        userModel.setFunction(userDTO.getFunction());
+        userModel.setTasks(userDTO.getTasks());
+
+        return userModel;
+    }
+
+    public UserDTO map(UserModel userModel) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userModel.getId());
+        userDTO.setName(userModel.getName());
+        userDTO.setMail(userModel.getMail());
+        userDTO.setAge(userModel.getAge());
+        userDTO.setRole(userModel.getRole());
+        userDTO.setFunction(userModel.getFunction());
+        userDTO.setTasks(userModel.getTasks());
+
+        return userDTO;
+    }
+
 }
