3cabd18f2dfe1c6da588059369288369b577ca0d
Matheus Gomes <you@example.com>
2025-04-15 08:56:39 -0300

    feat: Changes the controller route to list the ninjas and injects the dependency of the SERVIVCE and the REPOSITORY

--- src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserController.java
+++ src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserController.java
@@ -2,10 +2,16 @@ package dev.matheus.CadastroAPIJava10x.Users;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     @GetMapping("/boasvindas")
     public  String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
@@ -22,8 +28,8 @@ public class UserController {
     }
     // Show all users (READ)
     @GetMapping("/show")
-    public String showAllUsers() {
-        return "All users";
+    public List<UserModel> showAllUsers() {
+        return userService.showAllUsers();
     }
     // Change user data (UPDATE)
     @PutMapping("/changeID")
--- src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserService.java
+++ src/main/java/dev/matheus/CadastroAPIJava10x/Users/UserService.java
@@ -1,4 +1,20 @@
 package dev.matheus.CadastroAPIJava10x.Users;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Show all users
+    public List<UserModel> showAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
